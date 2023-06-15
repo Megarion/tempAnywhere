675832aa360c5fb842c3cb610ec908426af1b563
Megarion <andy181209x@gmail.com>
2023-06-15 23:19:11 +0700

    a

--- api/index.js
+++ api/index.js
@@ -1,20 +1,8 @@
 import axios from "axios";
 import rateLimit from 'express-rate-limit';
 const app = require('express')();
-import { v4 } from 'uuid';
+// import { v4 } from 'uuid';
 require('dotenv').config();
-
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
 
 const limiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 15 minutes
@@ -23,9 +11,9 @@ const limiter = rateLimit({
 	legacyHeaders: false,
 })
 
-app.use("/temp", limiter)
+app.use("/api", limiter)
 
-app.get("/temp", (req, res) => {
+app.get("/api", (req, res) => {
     const params = req.query;
     // @ts-ignore
     const KEY = process.env.VITE_KEY;
