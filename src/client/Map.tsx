c515b6ba416420b5c891e443ec38419434a4ede8
Megarion <andy181209x@gmail.com>
2023-06-16 12:22:23 +0700

    HELLOASDKA0DKDYTBYFSANMK,LD'/|@#4

--- src/client/Map.tsx
+++ src/client/Map.tsx
@@ -3,8 +3,8 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useMap } from 'react-leaflet';
 import Locate from "leaflet.locatecontrol";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '/@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '/@fortawesome/free-solid-svg-icons';
 
 // @ts-ignore
 function TempMark(props) {
